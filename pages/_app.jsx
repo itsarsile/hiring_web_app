e29968ae4b42101d36b9024d75a6d453570933a8
Arsile <ajiprio@hotmail.com>
2023-07-26 18:40:11 +0700

    feat: :art: Added corporate theme

---
+++ pages/_app.jsx
@@ -0,0 +1,9 @@
+import '@/styles/globals.css'
+
+export default function App({ Component, pageProps }) {
+  return (
+    <div data-theme='corporate'>
+      <Component {...pageProps} />
+    </div>
+  )
+}
